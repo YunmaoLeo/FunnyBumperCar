69f907731700b0fbe67d10753d9d0bba5aa46531
YunmaoLeo <biyll8@nottingham>
2023-08-16 22:28:40 +0800

    [feat(master): implement simple hook]

---
+++ FunnyBumperCar/Assets/Scripts/Addons/Hook.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    // private ConfigurableJoint joint;
+    private Rigidbody rb;
+    private Rigidbody attachedRb;
+
+    public bool isAttaching;
+    private Vector3 HookRelativePosition;
+    public bool isActive = false;
+    public Vector3 AttachedPosition;
+
+    private void Awake()
+    {
+        // joint = GetComponent<ConfigurableJoint>();
+        // rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isActive || isAttaching) return;
+        // rb.isKinematic = true;
+        attachedRb = other.attachedRigidbody;
+
+        AttachedPosition = other.ClosestPoint(transform.position);
+        isAttaching = true;
+        HookRelativePosition = other.transform.InverseTransformPoint(transform.position);
+        Debug.Log("OnHookCollisionEnter");
+    }
+
+    private void FixedUpdate()
+    {
+        if (isAttaching)
+        {
+            if (attachedRb != null)
+            {
+                transform.position = attachedRb.position + HookRelativePosition;
+                // rb.MovePosition(attachedRb.position + HookRelativePosition);
+                // attachedRb.AddForceAtPosition(joint.currentForce, rb.position);
+            }
+            else
+            {
+                transform.position = AttachedPosition;
+            }
+        }
+    }
+
+    public void AddForceOnHookRope(Rigidbody hookBase, float force)
+    {
+        if (isAttaching)
+        {
+            var forceVec = (transform.position - hookBase.position) * force;
+            if (attachedRb != null)
+            {
+                attachedRb.AddForce(-forceVec);
+            }
+            hookBase.AddForce(forceVec);
+        }
+    }
+
+    public void LoseHook()
+    {
+        attachedRb = null;
+        // rb.isKinematic = false;
+        isAttaching = false;
+        isActive = false;
+    }
+}
---
+++ FunnyBumperCar/Assets/Scripts/Addons/HookAddon.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HookAddon : AddonObject
+{
+    [Header("Properties")] [SerializeField]
+    private float maxStretchLength = 10f;
+
+    [SerializeField] private float maxDuration = 2f;
+    [SerializeField] private float hookStretchSpeed = 10f;
+    [SerializeField] private float retrieveSpeed = 20f;
+    [SerializeField] private float retrieveForce = 100f;
+
+    [SerializeField] private LineRenderer hookLine;
+    [SerializeField] private Transform hookBase;
+    [SerializeField] private Transform hookTransform;
+    [SerializeField] private Hook hook;
+    [SerializeField] private float minDistance = 0.2f;
+
+    [SerializeField] private float hookCD = 8f;
+    // [SerializeField] private ConfigurableJoint hookConfigJoint;
+    private float hookCDTimer = 5f;
+    private float retrieveTimer = 0f;
+
+    [SerializeField] private Rigidbody hookBaseRb;
+    private FixedJoint fixedJoint;
+    private Vector3 hookDefaultPosition;
+    private HookState hookState = HookState.Static;
+
+    enum HookState
+    {
+        Static,
+        IsFindingTarget,
+        IsRetrieving
+    }
+
+    public override void InitializeBasePlatformRigidbody(Rigidbody rigidbody)
+    {
+        base.InitializeBasePlatformRigidbody(rigidbody);
+        fixedJoint = GetComponent<FixedJoint>();
+        fixedJoint.connectedBody = rigidbody;
+
+        hookDefaultPosition = hookTransform.localPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateRope();
+
+        switch (hookState)
+        {
+            case HookState.Static:
+                hookCDTimer += Time.fixedDeltaTime;
+                break;
+            case HookState.IsFindingTarget:
+                StretchHook();
+                if (GetCurrentDistance() > maxStretchLength || hook.isAttaching)
+                {
+                    Retrieve();
+                }
+                break;
+            case HookState.IsRetrieving:
+                retrieveTimer += Time.fixedDeltaTime;
+                hook.AddForceOnHookRope(this.hookBaseRb, retrieveForce);
+                if (retrieveTimer > maxDuration)
+                {
+                    hook.LoseHook();
+                }
+
+                if (!hook.isAttaching)
+                {
+                    RetrieveHook();
+                }
+
+                if (GetCurrentDistance() < minDistance || Vector3.Dot(hookTransform.position - hookBase.position, hookBase.forward) > 0)
+                {
+                    hook.LoseHook();
+                    hookState = HookState.Static;
+                }
+                break;
+        }
+    }
+
+    private float GetCurrentDistance()
+    {
+        // var basePos = hookBase.position + hookConfigJoint.connectedAnchor;
+        var basePos = hookBase.position;
+        var hookPos = hookTransform.position;
+
+        return Vector3.Distance(basePos, hookPos);
+    }
+
+    private void StretchHook()
+    {
+        hookTransform.position += -transform.forward * (Time.fixedDeltaTime * hookStretchSpeed);
+    }
+    
+    private void RetrieveHook()
+    {
+        hookTransform.position -= -transform.forward * (Time.fixedDeltaTime * retrieveSpeed);
+    }
+    
+
+    private void EjectHook()
+    {
+        hook.isActive = true;
+        hookState = HookState.IsFindingTarget;
+        hookCDTimer = 0f;
+        // hookConfigJoint.targetPosition = new Vector3(0, 0, maxStretchLength);
+    }
+
+    private void Retrieve()
+    {
+        hookState = HookState.IsRetrieving;
+        // hookConfigJoint.targetPosition = new Vector3(0, 0, 0);
+        retrieveTimer = 0f;
+    }
+
+    public override void TriggerAddon(InputAction.CallbackContext context)
+    {
+        base.TriggerAddon(context);
+        if (hookCDTimer > hookCD)
+        {
+            EjectHook();
+        }
+    }
+
+    private void UpdateRope()
+    {
+        var p1 = hookBase.localPosition;
+        var p2 = hookTransform.localPosition;
+
+        hookLine.SetPosition(0, p1);
+        hookLine.SetPosition(1, p2);
+
+
+    }
+}
